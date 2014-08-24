e9bcf1cc2e8de3533141e71695a0378f23fd4127
laras126 <laras126@gmail.com>
2014-08-23 23:44:09 -0400

    get location and print closeby bars on form submit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Fly Solo
+
+Is a playground for Lara learning JavaScript and other stuff. Under the pretense of being an app to track bars that are good to go to alone.
+
+It has a really cute URL:
+
+flysolo.in/nyc
+flysolo.in/bos
+flysolo.in/pgh
+flysolo.in/sf
+flysolo.in/dc
+
+And more cities. Bare in mind none of that exists yet.
+
+Things I am learning with this project:
+* libsass
+* Foursquare API
+* Bourbon/Neat (which are older versions because of libsass)
+* Forms and JSON and stuff
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,59 @@
-'use strict';
-
 $(document).ready(function() {
 
-	$.ajax({
-		url: 'https://api.foursquare.com/v2/venues/explore?client_id=CKSDM1KYCKKBSXJUFZWW2PWB0YZRN0FPMQIRX0IL2UWHMCPW&client_secret=RTQWFLNZS4BYGOUU4MEKOXSFUZPU1PIJ4XP3HNYRZPBVVWDD&v=20130815&ll=40.7,-74&query=cocktail',
-		
-		success: function( data ) {
-			console.log(data.response.venues);
-			var items = [];
-			var places = data.response.venues;
-			$.each( places, function(key,val) {
-				var title = '<h3>'+val.name+'</h3>';
-				var phone = '<p>'+val.contact.phone+'</p>';
+	var latitude, longitude;
 
-				$('.items').append('<li>' + title + phone +'</li>');
-			} );
-		}
-	});
+	if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(searchFoursquare);
+    } else { 
+        $('#userLocation').html('Geolocation is not supported by this browser.');
+    }
 
 });
 
+function searchFoursquare(position) {
+
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+    
+    // This is probably not the correct way to do this
+	var query_val = $('#flySoloForm').serialize() + $('#queryInput').val();
+
+	// Add FourSquare authentication and what not
+	// Get the users lat/lon
+	// Add the 'query' value from the form
+    var fsq_data = { 
+		client_id: 'CKSDM1KYCKKBSXJUFZWW2PWB0YZRN0FPMQIRX0IL2UWHMCPW',
+		client_secret: 'IH3SNJ2GDP4M5OLESLWW5D23BV35TMNCUNOX3BES4RQ1O4GD',
+		v: '20130815',
+		ll: latitude+','+longitude,
+		query: query_val
+	};
+
+	var fsq_encoded = $.param(fsq_data);	
+
+
+	$('#flySoloForm').submit( function( event ) {
+
+		console.log(fsq_encoded);
+		$.ajax({
+			url: 'https://api.foursquare.com/v2/venues/search',
+			method: 'GET',
+			data: fsq_encoded,
+		})
+		.done( function(data) {
+			console.log(data);
+			var items = [];
+			var places = data.response.venues;
+			$.each( places, function(key,val) {
+				var title = val.name;
+				$('.items').append('<li>' + title + '</li>');
+			} );
+		});
+	
+		return false;
+	
+	});
+
+	$('#userLocation').html('Latitude: ' + latitude + '<br>Longitude: ' + longitude);
+
+}
